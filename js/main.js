9eb875286613d26fb194a48f5bfe9f84274c6d41
Matheus Cuba <matheusmcuba@gmail.com>
2018-10-27 23:46:29 -0300

    Finalizado mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-let template = '<div class="message {0}"><div class="message-content">{1}</div><div class="message-info">{2}</div></div>'
+let template = '<message-wrapper class="{0}"><message-content>{1}</message-content><message-info>{2}</message-info></message-wrapper>'
 let randomPharses = [
     'What is your favorite color?',
     'Great!',
     'How do you find me?'
 ];
+
 if (!String.format) {
     String.format = function(format) {
       var args = Array.prototype.slice.call(arguments, 1);
@@ -17,35 +18,40 @@ if (!String.format) {
   }
 
 $(function(){
+
     setTimeout(function(){
-       $('.chat-bubble').addClass('active');
-    }, 1000);
+        $('chat-bubble').addClass('loaded');
+        setTimeout(function(){
+           $('bubble-message').addClass('active');
+        }, 1000);
+    }, 500);
 
-    $(document).on('click', '.thumb:not(.up)', function(){
-      let self = this;
-       $('.chat-bubble').removeClass('active');
-
-       $(self).addClass('in-box');
-       setTimeout(function(){
-          $(self).addClass('up');
-          $('.box').addClass('active');
-          $('.chat-bubble').addClass('close');
-    $('.body-messages').scrollTop($('.body-messages')[0].scrollHeight);
-          setTimeout(() => {
-            $('.message').addClass('sended');      
-          }, 300);
-       }, 300)
+    $(document).on('click', 'chat-bubble:not(.up)', function(){
+        let self = this;
+        $('bubble-message').removeClass('active');
+        $(self).addClass('in-box');
+        setTimeout(function(){
+            $(self).addClass('up');
+            $('chat-backdrop').addClass('active');            
+            $('chat-box').addClass('active');
+            $('bubble-message').addClass('close');
+            $('chat-content').scrollTop($('chat-content')[0].scrollHeight);
+            setTimeout(() => {
+                $('message-wrapper').addClass('sended');      
+            }, 300);
+        }, 300)
     });
 });
 
     
-$(document).on('click', '.thumb.up', function(){
-    let self = this;
+$(document).on('click', '#close-chat', function(){
+    let self = $('chat-bubble');
+    $('chat-backdrop').removeClass('active');   
     $(self).removeClass('up');
-    $('.box').removeClass('active');
+    $('chat-box').removeClass('active');
     setTimeout(function(){
         $(self).removeClass('in-box');
-        $('.message').removeClass('sended'); 
+        $('message-wrapper').removeClass('sended'); 
     }, 300);
 });
 
@@ -60,16 +66,17 @@ $('#send').click(function(){
     NovaMensagem($('#userInput').val(),false);
     $('#userInput').val('');
 });
-$('.chat-bubble span').click(function(){
-    $('.chat-bubble').removeClass('active');
+
+$('bubble-message span').click(function(){
+    $('bubble-message').removeClass('active');
     setTimeout(function(){
-        $('.chat-bubble').addClass('close');
+        $('bubble-message').addClass('close');
     }, 300)
 });
 
 function NovaMensagem(mensagem, bot){
     let message = String.format(template, bot ? 'left' : 'right', mensagem, 'just now');
-    $('.body-messages').append(message);
-    $('.body-messages').scrollTop($('.body-messages')[0].scrollHeight);
-    $('.message').addClass('sended');
+    $('chat-content').append(message);
+    $('chat-content').scrollTop($('chat-content')[0].scrollHeight);
+    $('message-wrapper').addClass('sended');
 }
